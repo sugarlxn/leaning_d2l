0c2664de6d0a69e300b2865d0669c55bb4de6078
lxn_WSL <1933650668@qq.com>
2024-09-12 22:26:29 +0800

    add vgg.ipynb

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --name d2l --ipc=host --gpus=all -v /home/lxn/leaning_d2l:/leaning_d2l -p 8888:8888 lxn_d2l:1.0 /bin/bash
+docker run -it --name d2l --ipc=host --gpus=all -v /home/lxn/leaning_d2l:/leaning_d2l -p 8888:8888 lxn_d2l:1.0 /bin/bash
