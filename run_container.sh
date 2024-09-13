665f216bb96b732cc29d44d900045233d73b401c
lxn_WSL <1933650668@qq.com>
2024-09-13 23:24:36 +0800

    add 2nin net in net

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# run lxn_d2l:1.0 container and mount the host directory to the container
 docker run -it --name d2l --ipc=host --gpus=all -v /home/lxn/leaning_d2l:/leaning_d2l -p 8888:8888 lxn_d2l:1.0 /bin/bash
